4a4ecdb0148c47675b624463753dfa013c2aa4eb
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-08-24 15:09:07 +0200

    [Test] Add minimal test for templates inclusion.
    We probably want to test more than just them being able to compile, but
    this is a start.

--- test/src/tests.h
+++ test/src/tests.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  register_types.cpp                                                   */
+/*  tests.h                                                              */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,46 +28,25 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#include "register_types.h"
+#ifndef TESTS_H
+#define TESTS_H
 
-#include <godot/gdnative_interface.h>
+#include "godot_cpp/templates/cowdata.hpp"
+#include "godot_cpp/templates/hash_map.hpp"
+#include "godot_cpp/templates/hash_set.hpp"
+#include "godot_cpp/templates/hashfuncs.hpp"
+#include "godot_cpp/templates/list.hpp"
+#include "godot_cpp/templates/pair.hpp"
+#include "godot_cpp/templates/rb_map.hpp"
+#include "godot_cpp/templates/rb_set.hpp"
+#include "godot_cpp/templates/rid_owner.hpp"
+#include "godot_cpp/templates/safe_refcount.hpp"
+#include "godot_cpp/templates/search_array.hpp"
+#include "godot_cpp/templates/sort_array.hpp"
+#include "godot_cpp/templates/spin_lock.hpp"
+#include "godot_cpp/templates/thread_work_pool.hpp"
+#include "godot_cpp/templates/vector.hpp"
+#include "godot_cpp/templates/vmap.hpp"
+#include "godot_cpp/templates/vset.hpp"
 
-#include <godot_cpp/core/class_db.hpp>
-#include <godot_cpp/core/defs.hpp>
-#include <godot_cpp/godot.hpp>
-
-#include "example.h"
-#include "tests.h"
-
-using namespace godot;
-
-void initialize_example_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
-
-	ClassDB::register_class<ExampleRef>();
-	ClassDB::register_class<ExampleMin>();
-	ClassDB::register_class<Example>();
-}
-
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
-	}
-}
-
-extern "C" {
-
-// Initialization.
-
-GDNativeBool GDN_EXPORT example_library_init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
-	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
-
-	init_obj.register_initializer(initialize_example_module);
-	init_obj.register_terminator(uninitialize_example_module);
-	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-	return init_obj.init();
-}
-}
+#endif // TESTS_H
